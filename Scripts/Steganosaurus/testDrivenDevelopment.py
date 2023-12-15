646cdd2951f8d85f4b2985cd3b8bcf67d47edbcd
blake <99916948+nilsdeb@users.noreply.github.com>
2023-12-15 14:33:41 +0100

    15.2023 - suite test Nils #02

--- Scripts/Steganosaurus/testDrivenDevelopment.py
+++ Scripts/Steganosaurus/testDrivenDevelopment.py
@@ -1,33 +1,79 @@
 import unittest
-import sys
+import PIL
+import os
 import cubeDilution
 
 
+# chemin de base du dossier
+base_directory = os.getcwd()
 
 
-class Alphabet(unittest.TestCase):
+class TestAlphabet(unittest.TestCase):
+
+
 
     def setUp(self):
 
         self.message = "Hello World"
 
-        self.messageCoder = cubeDilution.translateBinary(self.message)
+        self.message_coder = cubeDilution.translateBinary(self.message)
 
-        self.messageDecoder = cubeDilution.translateAlphabetical(self.messageCoder)
-    
+        self.message_decoder = cubeDilution.translateAlphabetical(self.message_coder)
+
+        cubeDilution.encryptMessage(self.message,os.path.join(base_directory,"kenan.jpeg"),os.path.join(base_directory,"farouk.png"))
+
+        self.message_decrypter = cubeDilution.decryptMessage(os.path.join(base_directory,"farouk.png"))
+
+
+
     def test_translateBinary(self):
 
-        self.assertIsInstance(self.messageCoder, list)
+        self.assertIsInstance(self.message_coder, list)
 
-        for car in self.messageCoder:
+        for car in self.message_coder:
 
             self.assertLess(car, 2)
 
+
+
     def test_translateAlphabetical(self):
 
-        self.assertIsInstance(self.messageDecoder, str)
+        self.assertIsInstance(self.message_decoder, str)
 
-        self.assertEqual(self.messageDecoder, self.message)
+        self.assertEqual(self.message_decoder, self.message)
+
+
+
+    def test_encryptMessage(self):
+
+        self.assertTrue(os.path.join(base_directory,"farouk.png"))
+
+        image1 = PIL.Image.open(os.path.join(base_directory,"kenan.jpeg"))
+        image2 = PIL.Image.open(os.path.join(base_directory,"farouk.png"))
+
+        self.assertEqual(image1.size, image2.size)
+
+        differences_found = False
+
+        for x in range(image1.width):
+            for y in range(image1.height):
+                pixel1 = image1.getpixel((x, y))
+                pixel2 = image2.getpixel((x, y))
+                if pixel1 != pixel2:
+                    differences_found = True
+
+        self.assertTrue(differences_found)
+
+
+
+    def test_decryptMessage(self):
+
+        self.assertIsInstance(self.message_decoder, str)
+
+        self.assertEqual(self.message_decrypter[:len(self.message)], self.message)
+
+
+
 
 
 
